c919bfc25cfd8baf7857f3a01a0d89bd617bf6e0
Sunny <sunnylee945@hotmail.com>
2017-10-08 10:08:22 +0800

    Section 6: Services & Http -- Creating a Service.

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SandboxComponent } from './components/sandbox/sandbox.component';
+import { DataService } from './services/data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SandboxComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+    selector:'sandbox',
+    template:`
+        <h1> Hello World </h1>
+        <ul class="list-group">
+            <li class="list-group-item" *ngFor="let user of users"> {{ user }}  </li>
+        </ul>
+
+    `,
+    
+})
+
+export class SandboxComponent{
+    users:string[];
+    constructor(public dataService: DataService){
+        //console.log(this.dataService.getUsers());
+        this.users = this.dataService.getUsers();
+    }
+}
